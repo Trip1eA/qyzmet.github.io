94ad6cbda66a2bc8e5e5ac8cdcfb6a83734651b7
Trip1eA <53402529+Trip1eA@users.noreply.github.com>
2019-07-28 19:59:58 +0500

    rename

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$token = "978553508:AAEheY8odzWSH-6YDJHSAROf0jwBW3sRmTE";
+$bot = new \TelegramBot\Api\Client($token);
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - вывод справки';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
+?>
