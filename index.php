899cb94658933e36881ee5efbe75ef6a7fb6d671
Trip1eA <53402529+Trip1eA@users.noreply.github.com>
2019-07-28 20:35:41 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
-$url="https://api.telegram.org/bot978553508:AAEheY8odzWSH-6YDJHSAROf0jwBW3sRmTE/";
-//читаем результат из стандартного потока, в который PHP записывает полученные данные
-$a=file_get_contents('php://input');
-$content = json_decode($a);
-//получаем значение chat_id – идентификатор чата с пользователем, отправившим сообщение
-$chatID=$update['result'][0]['message']['chat']['id'];
-//Инициализируем новый запрос
-$ch = curl_init();
-//Формируем строку запроса – отправка пользователю сообщения «hello»
-$msg=$url."sendMessage?chat_id=".$chatID."&text=Hello";
-//Настраиваем запрос
-curl_setopt($con, CURLOPT_URL, $msg);
-curl_setopt($con, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($con, CURLOPT_HEADER, 0);
-//Выполняем запрос
-$output = curl_exec($con);
-//Закрываем запрос
-curl_close($con);
-?>
+/**
+ * Telegram Bot тупо бот access token и URL.
+ */
+$access_token = '978553508:AAEheY8odzWSH-6YDJHSAROf0jwBW3sRmTE';
+$api = 'https://api.telegram.org/bot' . $access_token;
+/**
+ * Задаём основные переменные.
+ */
+$output = json_decode(file_get_contents('php://input'), TRUE);
+@$chat_id = $output['message']['chat']['id'];
+@$message = $output['message']['text'];
+// команды тупо-бота
+switch($message) {
+    case '/start':
+    sendMessage($chat_id, "\xF0\x9F\x93\xA1 тупобот на связи!");
+    break;
+}
+
+function sendMessage($chat_id, $message) {
+  file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . '&parse_mode=html');
+}
