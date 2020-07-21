18be0cbd7968cac82849436ef6ab3ae851979a3f
Trip1eA <53402529+Trip1eA@users.noreply.github.com>
2020-07-22 03:21:51 +0500

    Delete off.php

--- off.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-echo "<h1>Успешно, вернем Вас обратно через 2 секунды...</h1>";
-header("refresh: 2;url=http://qazaq.zzz.com.ua/");
-?>
